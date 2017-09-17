64a171e00e33f4a5727736bd423f3869146e2233
Jonathan Lin <jonathan.fs.lin@gmail.com>
2017-09-17 16:36:32 -0400

    Poop button operational

--- Hypnos/app/src/main/java/com/artemis/hypnos/android/MainActivity.java
+++ Hypnos/app/src/main/java/com/artemis/hypnos/android/MainActivity.java
@@ -1,58 +1,52 @@
 package com.artemis.hypnos.android;
 
-import android.support.design.widget.TabLayout;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    private SectionsPagerAdapter mSectionsPagerAdapter;
+    private String babyName = "";
+    private Calendar babyDOB = Calendar.getInstance();
+    private String babyId = "";
+    private Calendar newDayTime = Calendar.getInstance();
 
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    private ViewPager mViewPager;
+    public final static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    private FirebaseAuth auth;
+    private DatabaseReference mDatabase;
+
+    private Button mBtnPooped;
+    private TextView mTxtPoopedLast;
+    private TextView mTxtPoopedToday;
+
+    private TextView mTxtBabyName;
+    private TextView mTxtBabyDob;
+    private TextView mTxtNewDayTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(mViewPager);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -63,100 +57,132 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-    }
+        babyName = "Tobias Lin";
+        babyDOB.set(2017, Calendar.MAY, 3, 0, 0, 0);
+        newDayTime.set(newDayTime.get(Calendar.YEAR),
+                newDayTime.get(Calendar.MONTH),
+                newDayTime.get(Calendar.DAY_OF_MONTH),
+                7, 0, 0); // new day is 7 am
+        try {
+            String hashString = babyName + ", " + dateFormat.format(babyDOB.getTime());
 
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(hashString.getBytes());
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+            byte byteData[] = md.digest();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "SECTION 1";
-                case 1:
-                    return "SECTION 2";
-                case 2:
-                    return "SECTION 3";
+            //convert the byte to hex format method 1
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < byteData.length; i++) {
+                sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
             }
-            return null;
+
+            babyId = sb.toString();
+            System.out.println("Digest(in hex format):: " + babyId);
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
         }
+
+        // connect to firebase
+        auth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        mBtnPooped = (Button) findViewById(R.id.btnPooped);
+        mTxtPoopedLast = (TextView) findViewById(R.id.txtLastPooped);
+        mTxtPoopedToday = (TextView) findViewById(R.id.txtTodayPooped);
+
+        mTxtBabyName = (TextView) findViewById(R.id.txtBabyName);
+        mTxtBabyDob = (TextView) findViewById(R.id.txtBabyDob);
+        mTxtNewDayTime = (TextView) findViewById(R.id.txtNewDayTime);
+
+        setHandles();
+        setUI();
+
+
+
+
+
+        // Get a reference to our posts
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference refTimePooped = database.getReference("baby/" + babyId + "/TimePooped");
+
+        // Attach a listener to read the data at our posts reference
+        refTimePooped.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                long latestTimestamp = 0;
+                int counterToday = 0;
+
+                long currentLimit = newDayTime.getTimeInMillis();
+
+                for (DataSnapshot child: dataSnapshot.getChildren()) {
+//                    Log.d("User key", child.getKey());
+//                    Log.d("User ref", child.getRef().toString());
+//                    Log.d("User val", child.getValue().toString());
+
+                    long currTimeStamp = Long.parseLong((String) child.getValue());
+
+                    if (currTimeStamp > latestTimestamp) {
+                        latestTimestamp = currTimeStamp;
+                    }
+
+                    if (currTimeStamp > currentLimit) {
+                        counterToday++;
+                    }
+                }
+
+                Calendar poopTime = Calendar.getInstance();
+                poopTime.setTimeInMillis(latestTimestamp);
+                if (latestTimestamp == 0) {
+                    mTxtPoopedLast.setText("Never pooped");
+                } else {
+                    mTxtPoopedLast.setText(dateFormat.format(poopTime.getTime()));
+                }
+                    mTxtPoopedToday.setText(String.valueOf(counterToday));
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
+            }
+        });
+    }
+
+    public String milliToString(long latestTimestamp) {
+        Calendar poopTime = Calendar.getInstance();
+        poopTime.setTimeInMillis(latestTimestamp);
+        return dateFormat.format(poopTime.getTime());
+    }
+
+    public void setUI() {
+        mTxtBabyName.setText(babyName);
+        mTxtBabyDob.setText(dateFormat.format(babyDOB.getTime()));
+        mTxtNewDayTime.setText(dateFormat.format(newDayTime.getTime()));
+    }
+
+    public void setHandles() {
+        mBtnPooped.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addEntryPoop();
+            }
+        });
+    }
+
+    private void addEntryPoop(){
+//        Date currentTime = Calendar.getInstance().getTime();
+        long currentTimeMs = Calendar.getInstance().getTimeInMillis();
+
+        mDatabase.child("baby").
+                child(babyId).
+                child("TimePooped").
+                push().
+                setValue(String.valueOf(currentTimeMs));
+    }
+
+    private void getLastPoop() {
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference("baby/" + babyId + "saving-data/fireblog/posts");
     }
 }
